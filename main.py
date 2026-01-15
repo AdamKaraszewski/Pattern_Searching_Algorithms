7a213906322473b15593ba74fe8a648ebbb8a4e6
akaraszewski <karaszewskiadam@outlook.com>
2026-01-15 21:41:22 +0100

    mp was deleted

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 from naive_pattern_search import naive_pattern_search as naive
 from sunday_pattern_search import sunday_pattern_search as sunday
-from morris_pratt_pattern_search import morris_pratt_pattern_search as mp
 from generate_string import generate_string as generator
 
 # O(T)
@@ -15,12 +14,11 @@ pattern_t = generator(alphabet, 5)
 x_axis_t = range(5, 9996)
 y_axis_naive_t = []
 y_axis_sunday_t = []
-y_axis_mp_t = []
 text_t = generator(alphabet, 5)
 for i in x_axis_t:
     y_axis_naive_t.append(naive(pattern_t, text_t)[1])
     y_axis_sunday_t.append(sunday(alphabet, pattern_t, text_t)[1])
-    y_axis_mp_t.append(mp(pattern_t, text_t)[1])
+    # y_axis_mp_t.append(mp(pattern_t, text_t)[1])
     text_t += random.choice(alphabet)
 
 # O(P)
@@ -28,12 +26,10 @@ text_p = generator(alphabet, 10000)
 x_axis_p = range(41)
 y_axis_naive_p = []
 y_axis_sunday_p = []
-y_axis_mp_p = []
 pattern_p = 'a'
 for i in x_axis_p:
     y_axis_naive_p.append(naive(pattern_p, text_p)[1])
     y_axis_sunday_p.append(sunday(alphabet, pattern_p, text_p)[1])
-    y_axis_mp_p.append(mp(pattern_p, text_p)[1])
     pattern_p += random.choice(alphabet)
 
 # O(A)
@@ -41,14 +37,12 @@ ascii_alphabet = list(string.ascii_lowercase)
 x_axis_a = range(1, 11)
 y_axis_naive_a = []
 y_axis_sunday_a = []
-y_axis_mp_a = []
 for i in x_axis_a:
     alphabet_a = ascii_alphabet[:i]
     pattern_a = generator(alphabet_a, 10)
     text_a = generator(alphabet_a, 1000)
     y_axis_naive_a.append(naive(pattern_a, text_a)[1])
     y_axis_sunday_a.append(sunday(alphabet_a, pattern_a, text_a)[1])
-    y_axis_mp_a.append(mp(pattern_a, text_a)[1])
 
 plt.figure()
 plt.xlabel('text size')
@@ -57,7 +51,6 @@ plt.title('O(text_size)')
 plt.grid()
 plt.plot(x_axis_t, y_axis_naive_t, label='Naive Pattern Search O(T)')
 plt.plot(x_axis_t, y_axis_sunday_t, label='Sunday Pattern Search O(T)')
-plt.plot(x_axis_t, y_axis_mp_t, label='Morris-Pratt Pattern Search O(T)')
 plt.legend()
 
 plt.figure()
@@ -67,7 +60,6 @@ plt.title('O(pattern_size)')
 plt.grid()
 plt.plot(x_axis_p, y_axis_naive_p, label='Naive Pattern Search O(P)')
 plt.plot(x_axis_p, y_axis_sunday_p, label='Sunday Pattern Search O(P)')
-plt.plot(x_axis_p, y_axis_mp_p, label='Morris-Pratt Pattern Search O(P)')
 plt.legend()
 
 plt.figure()
@@ -77,7 +69,6 @@ plt.title('O(alphabet_size)')
 plt.grid()
 plt.plot(x_axis_a, y_axis_naive_a, label='Naive Pattern Search O(A)')
 plt.plot(x_axis_a, y_axis_sunday_a, label='Sunday Pattern Search O(A)')
-plt.plot(x_axis_a, y_axis_mp_a, label='Morris-Pratt Pattern Search O(A)')
 plt.legend()
 
 plt.show()
